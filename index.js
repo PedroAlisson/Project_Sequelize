0b3f2136172a14c4fb1fd7a5e1cad0c4436116d8
PedroAlisson <pedrodevelope@gmail.com>
2020-03-03 19:09:05 -0300

    Struture

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: false}))
 
+app.get('/', (req,res) =>{
+    res.render('home')
+})
+
 
 
 app.listen(port,() => console.log('Start'))
