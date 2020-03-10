5a47917feb6c00554570ec4dd3bd4bab69b486ef
PedroAlisson <pedrodevelope@gmail.com>
2020-03-10 20:12:17 -0300

    routes

--- routes/routes.js
+++ routes/routes.js
@@ -3,5 +3,11 @@ const pessoasController = require('../controllers/pessoasController')
 const router = express.Router()
 
 router.get('/', pessoasController.index)
+router.get('/create', )
+router.post('/create', pessoasController.store)
+router.get('/update', )
+router.post('/update', pessoasController.updatePessoa)
+router.get('/delete', pessoasController.deletePessoa)
+
 
 module.exports = {router}
